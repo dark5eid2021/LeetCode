cbfe50792e9f027e56eb08d2db3d68c0801173b1
Jason Boykin <jasonboykin@gmail.com>
2025-03-25 19:28:38 -0500

    updated num islands

--- Meta/graphs/num_islands.py
+++ Meta/graphs/num_islands.py
@@ -1,58 +1,28 @@
-"""
-Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), 
-return the number of islands.
-
-An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
-You may assume all four edges of the grid are all surrounded by water.
-
-"""
-from typing import List
-
 class Solution:
-    def dfs(self, grid, i, j):
-        m, n = len(grid), len(grid[0])
-        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == '0':
-            return
-
-        grid[i][j] = '0'
-        self.dfs(grid, i + 1, j )
-        self.dfs(grid, i - 1, j)
-        self.dfs(grid, i, j + 1)
-        self.dfs(grid, i, j - 1)
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
-        count = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    count += 1
-                    self.dfs(grid, i, j)
-        return count
+        islands = 0
+        visited = set()
+        rows, cols = len(grid), len(grid[0])
 
+        def bfs(r, c):
+            q = deque()
+            visited.add((r, c))
+            q.append((r, c))
 
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
 
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1" and (r, c) not in visited:
+                        q.append((r, c))
+                        visited.add((r, c))
 
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    islands += 1
+                    bfs(r, c)
 
-
-
-
-
-
-
-
-"""
-Approach:
-Traversal Strategy:
-    Use either Depth-First Search (DFS) or Breadth-First Search (BFS) to explore each island starting from a '1'.
-    Mark visited cells to avoid recounting.
-Implementation Steps:
-    Iterate through all cells in the grid.
-    When a cell with '1' is found, increment the island count and start a DFS/BFS to mark all connected land cells as visited.
-    Continue until all cells are processed.
-Edge Cases:
-    Grid with all '0's (output: 0 islands).
-    Grid with all '1's (output: 1 island).
-
-
-"""
+        return islands
